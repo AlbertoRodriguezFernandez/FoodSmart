9cb719106d8a0581b84e0c71754c79087829ab64
Alberto Rodriguez Fernandez <arf170903@gmail.com>
2025-04-03 13:32:56 +0200

    Subiendo carpetas

---
+++ FoodSmart-Backend/src/main/java/com/FoodSmart/FoodSmart_Backend/FoodSmartBackendApplication.java
@@ -0,0 +1,16 @@
+package com.FoodSmart.FoodSmart_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSmartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSmartBackendApplication.class, args);
+		System.out.println("hola mundillo");
+	}
+
+}
+
+
---
+++ FoodSmart-Backend/src/test/java/com/FoodSmart/FoodSmart_Backend/FoodSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodSmart.FoodSmart_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
