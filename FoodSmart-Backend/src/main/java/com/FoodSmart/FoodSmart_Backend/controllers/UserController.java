939c73b39be2a98358e5ca0c08ab5b63f5d16e09
LauraZafra <laurazafraalarcos@gmail.com>
2025-04-04 12:07:35 +0200

    añadidos archivos Users.java, UsersRepository.java y modificado UserController en backend, todo ok

--- FoodSmart-Backend/src/main/java/com/FoodSmart/FoodSmart_Backend/controllers/UserController.java
+++ FoodSmart-Backend/src/main/java/com/FoodSmart/FoodSmart_Backend/controllers/UserController.java
@@ -3,3 +3,27 @@ package com.FoodSmart.FoodSmart_Backend.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.FoodSmart.FoodSmart_Backend.model.Users;
+import com.FoodSmart.FoodSmart_Backend.repository.UsersRepository;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:4200") // Para permitir peticiones desde Angular
+public class UserController {
+
+    private final UsersRepository usersRepository;
+
+    public UserController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @GetMapping
+    public List<Users> obtenerUsuarios() {
+        return usersRepository.findAll();
+    }
+}
---
+++ FoodSmart-Backend/src/main/java/com/FoodSmart/FoodSmart_Backend/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.FoodSmart.FoodSmart_Backend.repository;
+
+import com.FoodSmart.FoodSmart_Backend.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
